bf3cc6def4cd87141e1c480c7576a1f22eb29481
bbondarets <25823758+bbondarets@users.noreply.github.com>
2018-10-01 14:36:14 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -198,9 +198,8 @@ export class RavenErrorHandler implements ErrorHandler {
       useClass: AuthInterceptor,
       multi: true
     },
-    { provide: ErrorHandler, 
-      useClass: RavenErrorHandler 
-    }],
+    { provide: ErrorHandler, useClass: RavenErrorHandler }
+    ],
   bootstrap: [AppComponent],
 
 })
