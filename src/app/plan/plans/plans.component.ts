cffd960d138366e095ee3b3fe0004e342004a997
Sofia <flyssoffia@gmail.com>
2018-11-05 11:43:06 +0200

    ability delete plans

--- src/app/plan/plans/plans.component.ts
+++ src/app/plan/plans/plans.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Plan } from '../../common/models/plan';
+import { AuthService } from '../../common/services/auth.service';
 import { PlanService } from '../../common/services/plan.service';
 
 @Component({
@@ -9,9 +10,22 @@ import { PlanService } from '../../common/services/plan.service';
 })
 export class PlansComponent implements OnInit {
   plans: Plan[];
-  constructor(private planService: PlanService) {
-    planService.getPlans().subscribe(x => this.plans = x);
+  hasPermisionsToDelete = false;
+  id: number;
+  index: number;
+  constructor(private planService: PlanService, private authService: AuthService) {
+    planService.getPlans().subscribe( (x: Plan[]) => this.plans = x);
   }
   ngOnInit() {
+    if (this.authService.isAdmin() || this.authService.isMentor()) {
+      this.hasPermisionsToDelete = true;
+    }
+  }
+
+  onDelete(id: number): void {
+    this.id = id;
+    this.planService.deletePlanById(this.id).subscribe();
+    this.index = this.plans.findIndex((plan: Plan) => plan.Id === id);
+    this.plans.splice(this.index, 1);
   }
 }
